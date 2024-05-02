efc4fc9de7808b00dec40c16d66547fe31114d72
Chanaka Rathnayaka <chanakadrathnayaka@gmail.com>
2024-05-02 02:58:07 -0700

    Update java_version_manager.sh

--- java_version_manager.sh
+++ java_version_manager.sh
@@ -6,6 +6,10 @@ jdk (){
     version="$2";
     use;
     ;;
+  set)
+    version="$2";
+    set;
+    ;;
   ls | list)
     list;
     ;;
@@ -22,6 +26,14 @@ jdk (){
     java -version;
   }
 
+  set(){
+    echo "This is a permanent change untill you change it and it will be applied to the system. Continue? (y/N): " && read confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || return;
+
+    oldFixedJH=$(cat ~/.zshrc 2>&1 | grep -Eo 'export JAVA_HOME=.+');
+    sed -iE "s#$oldFixedJH#export JAVA_HOME=$(/usr/libexec/java_home -v "$version")#g" ~/.zshrc
+    use;
+  }
+
   list(){
     for i in $(/usr/libexec/java_home -V 2>&1 | grep -Eo '\s+[0-9\._]+ \(x86_64\)' | sed -E 's/ |\(x86_64\)//g'); do
       echo "$i";
@@ -33,8 +45,10 @@ jdk (){
   }
 
   help(){
-    echo "jdk use <version>     Set the JDK to given version";
+    echo "jdk use <version>     Use the JDK to given version, New version will be available only in current shell (session)";
     echo "                      ex: jdk use 21, jdk use 1.8";
+    echo "jdk set <version>     Set the JDK to given version, New version will be permanently set for the system";
+    echo "                      ex: jdk set 21, jdk set 1.8";
     echo "jdk list              List down installed JDKs, Use the values from this list to set the JDK";
     echo "jdk current           Show the current active JDK version"
     echo "jdk help              Display the list";
